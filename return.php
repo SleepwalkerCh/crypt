9390ea6ab773c24cc6b1956e1120796097f27569
HopeKing1997 <834711075@qq.com>
2017-06-05 19:50:05 +0800

    试一试吧

---
+++ return.php
@@ -0,0 +1,64 @@
+<?php
+	$name=$_POST['city1'];
+	$time=$_POST['city2'];
+	$sign=$_POST['city3'];
+
+	$number;
+	$decrypted = "";
+	if($name == 'AMY')
+	{
+		$number = 11111;
+		$public_key = "./pem/1-public.pem";
+		$pu_key = openssl_pkey_get_public(file_get_contents($public_key));//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id 
+		openssl_public_decrypt($sign,$decrypted,$pu_key);//私钥加密
+		$decrypted = base64_encode($decrypted);//
+	}
+	else if($name == 'BOB')
+	{
+		$number = 22222;
+		$public_key = "./pem/2-public.pem";
+		$pu_key = openssl_pkey_get_public(file_get_contents($public_key));//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id 
+		openssl_public_decrypt($sign,$decrypted,$pu_key);//私钥加密
+		$decrypted = base64_encode($decrypted);//
+	}
+	else if($name == 'CORAL')
+	{
+		$number = 33333;
+		$public_key = "./pem/3-public.pem";
+		$pu_key = openssl_pkey_get_public(file_get_contents($public_key));//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id 
+		openssl_public_decrypt($sign,$decrypted,$pu_key);//私钥加密
+		$decrypted = base64_encode($decrypted);//
+	}
+	else
+		$number = 00001;
+
+	$mysql_server_name='localhost';
+	$mysql_username='root'; 
+	$mysql_password='root'; 
+	$mysql_database='authorization'; 
+
+	$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting"); 
+	mysql_query("set names 'utf8'"); 
+	mysql_select_db($mysql_database, $conn); //打开数据库
+	$sql = "select shop_au_code from shop_list where shop_num = $number"; //SQL语句
+	$result = mysql_query($sql, $conn); //查询
+	$num = mysql_affected_rows();
+	$grade;
+	if($num != 0)
+	{
+		$grade = mysql_result($result, 0);
+		$v = $time.$grade;
+		if($number == 00001)
+			$a = '没有该商店！';
+		else
+		{
+			if($v == $decrypted)
+				$a = '退货成功！';
+			else
+				$a = '退货失败！';
+		}
+	}
+	else
+		$a = '退货失败！';
+	echo $a;
+?>
--- return1.php
+++ return1.php
@@ -1,4 +1,7 @@
 <?php
+	$sign=$_POST['city1'];
+	$time=$_POST['city2'];
+
 	$number=11111;
 
 	$mysql_server_name='localhost';
@@ -18,17 +21,17 @@
 	else
 		$grade = 'asdfg';
 
-	$private_key = "./pem/1-private.pem";
-	$pi_key = openssl_pkey_get_private(file_get_contents($private_key));//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id
-	$time = time();//原始数据
-	$data = $time.$grade;
-	$encrypted = ""; 
-	openssl_private_encrypt($data,$encrypted,$pi_key);//私钥加密
-	$encrypted = base64_encode($encrypted);//
-	
-	header("Content-Type: text/plain");
-	header("Accept-Ranges: bytes");
-	header("Accept-Length: ".strlen($encrypted));
-	header("Content-Disposition: attachment; filename=$data.txt");
-	echo $encrypted;
+	$public_key = "./pem/1-public.pem";
+	$pu_key = openssl_pkey_get_public(file_get_contents($public_key));//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id
+	$decrypted = ""; 
+	openssl_public_decrypt($sign,$decrypted,$pu_key);//私钥加密
+	$decrypted = base64_encode($decrypted);//
+
+	$v = $time.$grade;
+	$a;
+	if($v == $decrypted)
+		$a = '退货成功！';
+	else
+		$a = '退货失败！';
+	echo $a;
 ?>
